ac4e0c441d236e079d527ef953400ac7cfd64d79
Ceduardoz <167792627+Ceduardoz@users.noreply.github.com>
2025-04-30 02:53:37 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Portfolio-Pessoal
+Este repositório contém o código-fonte do meu portfólio pessoal desenvolvido com HTML, CSS e JavaScript. O objetivo é apresentar meus principais projetos, habilidades como desenvolvedor front-end e facilitar o contato profissional. O site é responsivo, leve e com foco na usabilidade.
---
+++ assets/js/script.js
@@ -0,0 +1,58 @@
+//      INICIAR A PAGINA
+const frasePrincipal = document.querySelector('.frasePrincipal');
+const avatar = document.querySelector('.avatar');
+
+frasePrincipal.classList.add('show');
+frasePrincipal.style.transition = 'all 1s ease';
+
+frasePrincipal.addEventListener('transitionend', () => {
+    avatar.style.transition = 'all 1s ease';
+    avatar.classList.add('show');
+})
+
+//            SCROLL
+const boxes = document.querySelectorAll('.box');
+let timeout;
+
+window.addEventListener('scroll', () => {
+    clearTimeout(timeout);
+    timeout = setTimeout(() => {
+        boxes.forEach((box) => {
+            const boxTop = box.getBoundingClientRect().top;
+            const triggerBottom = window.innerHeight * 0.8;
+
+            if (boxTop < triggerBottom) {
+                box.classList.add('show');
+                box.style.transition = 'all 1s ease';
+            } else {
+                box.classList.remove('show');
+            }
+        });
+    }, 20);
+});
+
+//          MODO CLARO/ESCURO
+const btnDarkMode = document.querySelector('.navbar button');
+const body = document.body;
+const logoImg = document.querySelector('.logo');
+
+btnDarkMode.addEventListener('click', () => {
+    body.classList.toggle('modoClaro');
+
+    const icon = btnDarkMode.querySelector('i');
+
+    if (body.classList.contains('modoClaro')) {
+        icon.classList.remove('fa-moon');
+        icon.classList.add('fa-sun');
+        icon.style.color = '#31c79c';
+        logoImg.src = '/assets/img/logoLight.png';
+    } else {
+        icon.classList.remove('fa-sun');
+        icon.classList.add('fa-moon');
+        icon.style.color = '#8AFAD8';
+        logoImg.src = 'assets/img/logoDark.png';
+    }
+
+});
+
+
